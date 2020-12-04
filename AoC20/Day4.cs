cf29ff77836d0550ca9be4752dbafd3afc58d051
Mang'il <langlois.clement@gmail.com>
2020-12-04 13:23:56 +0100

    Day 4 part 1

---
+++ AoC20/Day4.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC20
+{
+    class Day4
+    {
+        List<Dictionary<string, string>> listPassport = new List<Dictionary<string, string>>();
+
+        public List<Dictionary<string, string>> ListPassport { get => listPassport; set => listPassport = value; }
+
+        public Day4(string addr)
+        {
+            if (!System.IO.File.Exists(addr))
+            {
+                Console.WriteLine("FILE NOT FOUND");
+                listPassport = null;
+            }
+            else
+            {
+                string[] data = System.IO.File.ReadAllLines(addr);
+                int i = 0;
+                listPassport.Add(new Dictionary<string, string>());
+                while (i < data.Length)
+                {
+                    if (data[i] == "") listPassport.Add(new Dictionary<string, string>());
+                    else
+                    {
+                        string[] temp = data[i].Split(' ');
+                        foreach (string s in temp)
+                        {
+                            listPassport[listPassport.Count - 1][s.Split(':')[0]] = s.Split(':')[1];
+                        }
+                    }
+                    i++;
+                }
+            }
+        }
+
+        public int NbrValide()
+        {
+            int r = 0;
+            if (listPassport == null) return -1;
+            foreach (var pass in listPassport)
+            {
+                if (pass.Count == 8) r++;
+                else if (pass.Count == 7 && !pass.ContainsKey("cid")) r++;
+            }
+            return r;
+        }
+
+    }
+}
--- AoC20/Program.cs
+++ AoC20/Program.cs
@@ -14,9 +14,10 @@ namespace AoC20
             Console.WriteLine("Hello world");
             //Console.WriteLine(Day1.CalculeSolutionFor3("../../Input/inputD1.txt"));
             //Console.WriteLine(Day2.NbrPasswordCorige("../../Input/inputD2.txt"));
-            Day3 test = new Day3("../../Input/inputD3.txt");
+            /*Day3 test = new Day3("../../Input/inputD3.txt");
             uint a = test.Reponse(1, 1), b = test.Reponse(1, 3), c = test.Reponse(1, 5), d = test.Reponse(1, 7), e = test.Reponse(2, 1);
-            Console.WriteLine(a*b*c*d*e);
+            Console.WriteLine(a*b*c*d*e);*/
+            Console.WriteLine(new Day4("../../Input/inputD4.txt").NbrValide());
             Console.ReadLine();
         }
     }
