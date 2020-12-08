32966dbae5c0922785267bd447c25525625c0316
Mang'il <langlois.clement@gmail.com>
2020-12-08 14:57:34 +0100

    Day 8 part 1

---
+++ AoC20/Day8.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC20
+{
+    class Instruction
+    {
+        string typeInst;
+        int valeur;
+        bool exe = false;
+
+        public Instruction(string type, int valeur)
+        {
+            this.typeInst = type;
+            this.valeur = valeur;
+        }
+
+        public int Valeur { get => valeur; set => valeur = value; }
+        public bool Exe { get => exe; set => exe = value; }
+        internal string TypeInst { get => typeInst; set => typeInst = value; }
+    }
+
+
+    class Day8
+    {
+        List<Instruction> prog = new List<Instruction>();
+
+        public Day8(string addr)
+        {
+            if (!System.IO.File.Exists(addr))
+            {
+                prog = null;
+                Console.WriteLine("FILE NOT FOUND");
+            }
+            else
+            {
+                string[] data = System.IO.File.ReadAllLines(addr);
+                foreach (string s in data)
+                {
+                    string[] temp = s.Split(' ');
+                    prog.Add(new Instruction(temp[0], int.Parse(temp[1])));
+                }
+            }
+        }
+
+        public int Execut()
+        {
+            int r = 0;
+            for (int i = 0; i < prog.Count;)
+            {
+                if (prog[i].Exe) return r;
+                else
+                {
+                    prog[i].Exe = true;
+                    switch (prog[i].TypeInst)
+                    {
+                        case "acc":
+                            r += prog[i].Valeur;
+                            i++;
+                            break;
+                        case "jmp":
+                            i += prog[i].Valeur;
+                            break;
+                        default:
+                            i++;
+                            break;
+                    }
+                }
+            }
+            return r;
+        }
+    }
+}
--- AoC20/Program.cs
+++ AoC20/Program.cs
@@ -12,19 +12,37 @@ namespace AoC20
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
+            //DAY 1
             //Console.WriteLine(Day1.CalculeSolutionFor3("../../Input/inputD1.txt"));
+
+            //DAY 2
             //Console.WriteLine(Day2.NbrPasswordCorige("../../Input/inputD2.txt"));
+
+            //DAY 3
             /*Day3 test = new Day3("../../Input/inputD3.txt");
             uint a = test.Reponse(1, 1), b = test.Reponse(1, 3), c = test.Reponse(1, 5), d = test.Reponse(1, 7), e = test.Reponse(2, 1);
             Console.WriteLine(a*b*c*d*e);*/
+
+            //DAY 4
             //Console.WriteLine(new Day4("../../Input/inputD4.txt").Verrif());
+
+            //DAY 5
             /*Day5 test = new Day5("../../Input/inputD5.txt");
             Console.WriteLine(test.GetSeat());*/
+
+            //DAY 6
             //Console.WriteLine(new Day6("../../Input/inputD6.txt",2).NbrRep);
-            Day7 day7 = new Day7("../../Input/inputD7.txt");
+
+            //DAY 7
+            //Day7 day7 = new Day7("../../Input/inputD7.txt");
             //day7.RechercheParent("shiny gold", "shiny gold");
             //Console.WriteLine(day7.RepPart1.Count);
-            Console.WriteLine(day7.NbrEnfant("shiny gold"));
+            //Console.WriteLine(day7.NbrEnfant("shiny gold"));
+
+            //DAY 8
+            Day8 day8 = new Day8("../../Input/inputD8.txt");
+            Console.WriteLine(day8.Execut());
+
             Console.ReadLine();
         }
     }
